d696a608f4cefd7022f5903eb0bafab5e22819b9
eSTee3 <tagsxa@yahoo.com>
2023-02-03 17:22:21 -0600

    added express-session dependency

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers'); 
 const helpers = require('./utils/helpers'); 
 const express = require('express'); 
-const session = require('express-session'); 
+const session = require('express-session');
 const sequelize = require('./config/connection'); 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
